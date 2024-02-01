53ce69eb665357d00a7a87868718cfc910519b0d
refatbaderkhan <refatbaderkhan@gmail.com>
2024-02-02 01:23:36 +0200

    <server> updated productsSchema keys and refs of users, suppliers, and geographical sub schemas

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -26,7 +26,34 @@ const pricesSchema = new mongoose.Schema({
   maxPrice : Number
 });
 
+const citiesSchema = new mongoose.Schema({
+  cityName: String,
+});
+
+const countriesSchema = new mongoose.Schema({
+  countryName: String,
+});
+
+const suppliersSchema = new mongoose.Schema({
+  supplierName: String,
+  cityId : { type: mongoose.Schema.Types.ObjectId, ref: 'Cities' },
+  countryId : { type: mongoose.Schema.Types.ObjectId, ref: 'Countries' },
+});
+
+const weekNumbersSchema = new mongoose.Schema({
+  weekNumber: Number,
+});
+
+const userNamesSchema = new mongoose.Schema({
+  userName: String,
+});
+
 const productsSchema = new mongoose.Schema({
+  weekNumberId : { type: mongoose.Schema.Types.ObjectId, ref: 'WeekNumbers' },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'UserNames' },
+  cityId : { type: mongoose.Schema.Types.ObjectId, ref: 'Cities' },
+  countryId : { type: mongoose.Schema.Types.ObjectId, ref: 'Countries' },
+  supplierId : { type: mongoose.Schema.Types.ObjectId, ref: 'Suppliers' },
   groupId : { type: mongoose.Schema.Types.ObjectId, ref: 'ProductGroups' },
   groupVarietyId : { type: mongoose.Schema.Types.ObjectId, ref: 'ProductGroupVarities' },
   originId : { type: mongoose.Schema.Types.ObjectId, ref: 'Origins' },
@@ -41,6 +68,11 @@ const Origins = mongoose.model('Origins', originsSchema);
 const PurchaseUoMs = mongoose.model('PurchaseUoMs', purchaseUoMsSchema);
 const Sizes = mongoose.model('Sizes', sizesSchema);
 const Products = mongoose.model('Products', productsSchema);
+const Cities = mongoose.model('Cities', citiesSchema);
+const Countries = mongoose.model('Countries', countriesSchema);
+const Suppliers = mongoose.model('Suppliers', suppliersSchema);
+const WeekNumbers = mongoose.model('WeekNumbers', weekNumbersSchema);
+const UserNames = mongoose.model('UserNames', userNamesSchema);
 
 module.exports = {  
   ProductGroups,
@@ -49,5 +81,10 @@ module.exports = {
   PurchaseUoMs,
   Sizes,
   Products,
+  Cities,
+  Countries,
+  Suppliers,
+  WeekNumbers,
+  UserNames
 };
 
