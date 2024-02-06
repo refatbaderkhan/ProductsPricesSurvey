c800606828a0695a13c646a0183b25ddf636fd54
refatbaderkhan <refatbaderkhan@gmail.com>
2024-02-06 12:39:12 +0200

    <client> added database git functions to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+import React, {useEffect, useState } from "react";
 import DateAndUsernameSection from "components/ui/DateAndUsernameSection";
 import LocationAndSupplierSection from "components/ui/LocationAndSupplierSection";
 import ProductGroupSection from "components/ui/ProductGroupSection";
 import ProductDetailsSection from "components/ui/ProductDetailsSection";
 import ProductPriceSection from "components/ui/ProductPriceSection";
+import { getProductGroups,
+         getProductGroupVarieties,
+         getOrigins,
+         getPurchaseUoMs,
+         getSizes,
+         getCountries,
+         getCities, 
+         getSuppliers, 
+         getWeekNumbers, 
+         getUserNames, 
+         getProducts 
+       } from "api/services";
 
 
 function App() {
+
+
   return (
     <div className="w-full">
       <DateAndUsernameSection />
